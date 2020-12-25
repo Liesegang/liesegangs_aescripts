d4395cfd8a78a57f9fbe1b4ebabefbefc2c4594f
liesegang <yyama0704@gmail.com>
2020-12-26 04:02:34 +0900

    add:build script

---
+++ scripts/build.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# build release
+
+import os
+import zipfile
+
+targetDir = "../src/"
+distDir = "../dist"
+name = "liesegangs_aescripts"
+version = "1.0.0"
+exclude = [".psd"]
+
+def main():
+  print("Version?")
+  print(">", end="")
+  version = input()
+  zipFile = zipfile.ZipFile(f'{distDir}/{name}_{version}.zip', 'w', zipfile.ZIP_DEFLATED)
+  for root, dirs, files in os.walk(targetDir):
+    for file in files:
+      print(os.path.splitext(file))
+      if(os.path.splitext(file)[1] in exclude):
+        continue
+      zipFile.write(os.path.join(root, file),
+        os.path.relpath(
+          os.path.join(root, file),
+          os.path.join(targetDir, '..')
+        )
+      )
+  zipFile.close()
+
+if __name__ == "__main__":
+  main()
