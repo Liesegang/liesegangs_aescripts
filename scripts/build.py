e8d1f6f08815515a6feab9b10137242885d5c99e
liesegang <yyama0704@gmail.com>
2020-12-26 04:04:46 +0900

    refactor[scripts/build.py]: use with when open zip

--- scripts/build.py
+++ scripts/build.py
@@ -14,19 +14,19 @@ def main():
   print("Version?")
   print(">", end="")
   version = input()
-  zipFile = zipfile.ZipFile(f'{distDir}/{name}_{version}.zip', 'w', zipfile.ZIP_DEFLATED)
-  for root, dirs, files in os.walk(targetDir):
-    for file in files:
-      print(os.path.splitext(file))
-      if(os.path.splitext(file)[1] in exclude):
-        continue
-      zipFile.write(os.path.join(root, file),
-        os.path.relpath(
-          os.path.join(root, file),
-          os.path.join(targetDir, '..')
+
+  with zipfile.ZipFile(f'{distDir}/{name}_{version}.zip', 'w', zipfile.ZIP_DEFLATED) as zipFile:
+    for root, dirs, files in os.walk(targetDir):
+      for file in files:
+        print(os.path.splitext(file))
+        if(os.path.splitext(file)[1] in exclude):
+          continue
+        zipFile.write(os.path.join(root, file),
+          os.path.relpath(
+            os.path.join(root, file),
+            os.path.join(targetDir, '..')
+          )
         )
-      )
-  zipFile.close()
 
 if __name__ == "__main__":
   main()
