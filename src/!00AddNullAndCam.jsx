11a99f81650bcc7bfd93d5807ed7e679629df790
liesegang <yyama0704@gmail.com>
2020-12-26 01:46:40 +0900

    add 3 shortcut scripts

---
+++ src/!00AddNullAndCam.jsx
@@ -0,0 +1,52 @@
+// ToggleStroke
+// Ver 1.0.1
+// Liesegang 2020 (yyama0704@gmail.com)
+'use strict';
+
+(function (){
+    function complaydate(){
+        comp = app.project.activeItem;
+        if(comp != null){
+            complay = comp.layers;
+            selay = comp.selectedLayers;
+            selaylength = selay.length;
+            if(selaylength != 0)
+                selayindex = selay[0].index;
+            else
+                selayindex = 1;
+            fps = comp.frameRate;
+        }else{
+            alert("Please access compsiton");
+            fps = 30;
+        }
+    }
+    function Addtodur(){
+        Din = 0; 
+        Dout = comp.duration;
+    }
+    function Addtoset(){
+        selay = comp.selectedLayers;
+        selay[0].inPoint = Din;
+        selay[0].outPoint = Dout;
+        if(selayindex > 1){
+            selay[0].moveAfter(comp.layer(selayindex));
+        }
+    }
+
+    function AddNullCamera(){
+        complaydate();
+        Addtodur();
+        complay.addCamera("camera", [-comp.width/2 ,- comp.height/2]);
+        var Camera = comp.selectedLayers[0];
+        Addtoset();
+        complaydate();
+        Addtodur();
+        complay.addNull().threeDLayer = true;
+        var Null = comp.selectedLayers[0];
+        Null.name= "camera_ctrl";
+        Addtoset();
+        Camera.parent = Null;
+    }
+
+    AddNullCamera();
+})()
---
+++ src/!02ToggleStroke.jsx
@@ -0,0 +1,56 @@
+// ToggleStroke
+// Ver 1.0.0
+// Liesegang 2020 (yyama0704@gmail.com)
+'use strict';
+
+(function (){
+    function map(arr, func){
+        var ret = [];
+        for(var i = 0; i < arr.length; i++){
+            ret.push(func(arr[i], i));
+        }
+        return ret;
+    }
+
+    function mapAllProperties(contents, func){
+        if(contents.numProperties !== undefined) {
+            for(var i = 1; i <= contents.numProperties; i++){
+                func(contents.property(i));
+            }
+        }
+    }
+
+    function includes(arr, x){
+        var ret = false;
+        map(arr, function (xx){
+            if(xx == x){
+                ret = true;
+            }
+        })
+        return ret;
+    }
+
+    function walkObjects(contents, matchName, func, args, only){
+        $.writeln(contents.matchName);
+        if(contents.matchName == matchName) func(contents);
+        if(only === undefined || includes(only, contents.matchName)){
+            mapAllProperties(contents, function(x){
+                walkObjects(x, matchName, func, args, only);
+            })
+        }
+    }
+
+    comp = app.project.activeItem;
+    if(comp == null) return;
+
+    selay = comp.selectedLayers;
+    if(selay.length == 0) return;
+
+    map(selay, function(layer, index){
+        if(!(layer instanceof ShapeLayer)) return;
+        var contents = layer.property("ADBE Root Vectors Group");
+        walkObjects(contents, "ADBE Vector Graphic - Stroke", function(obj){
+            obj.enabled ^= true;
+        }, undefined, ["ADBE Vector Group", "ADBE Vectors Group", "ADBE Root Vectors Group"]);
+    });
+})();
